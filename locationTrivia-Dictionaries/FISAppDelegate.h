fcdeb2eaf2a903c56789d9c2379f6cda8d73938c
wojunfeng <wjun1924@gmail.com>
2015-09-04 10:08:17 -0400

    prework locationTrivia

--- locationTrivia-Dictionaries/FISAppDelegate.h
+++ locationTrivia-Dictionaries/FISAppDelegate.h
@@ -17,5 +17,10 @@
  *  Declare your method names here.
  
  */
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length;
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude;
+- (NSArray *)namesOfLocations:(NSArray *)locations;
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary;
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations;
 
 @end
