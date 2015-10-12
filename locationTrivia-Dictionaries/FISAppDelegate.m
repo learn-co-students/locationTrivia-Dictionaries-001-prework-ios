a5e658bd4a22f57cce4094b755fdeb60e97fe62e
jspringer <jsnsprngr@gmail.com>
2015-10-11 22:44:13 -0400

    LOCATION TRIVIA - DICTIONARIES

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -16,4 +16,59 @@
  
  */
 
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+        if (length > [location[@"name"] length]) {
+                length = [location[@"name"] length];
+            }
+    
+        NSString *newString = [location[@"name"] substringToIndex:length];
+    
+        return newString;
+    }
+
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude{
+    
+        NSMutableDictionary *locationWithName = [[NSMutableDictionary alloc]init];
+    
+        locationWithName[@"name"] = name;
+        locationWithName[@"latitude"] = @(latitude);
+        locationWithName[@"longitude"] = @(longitude);
+        return locationWithName;
+    }
+
+- (NSArray *)namesOfLocations:(NSArray *)locations{
+        NSMutableArray *locationNames = [[NSMutableArray alloc]init];
+    
+        for (NSDictionary *location in locations){
+                [locationNames addObject:location[@"name"]];
+            }
+    
+        return locationNames;
+    };
+
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary{
+        BOOL dictIsValidLocation = NO;
+    
+        if ([ [dictionary allKeys] containsObject:@"name"] &&
+                     [ [dictionary allKeys] containsObject:@"latitude"] &&
+                     [ [dictionary allKeys] containsObject:@"longitude"] &&
+                     [dictionary count] == 3 ) {
+        
+                dictIsValidLocation = YES;
+            }
+    
+    
+        return dictIsValidLocation;
+    }
+
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations{
+        for (NSDictionary *location in locations){
+                if ([location[@"name"] isEqualToString:name]) {
+                        return location;
+                    }
+            }
+    
+        return nil;
+    }
+
 @end
