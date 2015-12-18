b75775dd8af46a1a8c48e739db94d29a25a06346
Shena Yoshida <sheyoshida@gmail.com>
2015-12-18 17:59:11 -0500

    Done.

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -12,30 +12,58 @@
 
 - (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location
                                       toLength:(NSUInteger)length {
-    
-    return nil;
+    if (length > [location[@"name"] length]) {
+        length = [location[@"name"] length];
+    }
+    NSString *locationString = [location[@"name"] substringToIndex:length];
+    return locationString;
 }
 
 - (NSDictionary *)dictionaryForLocationWithName:(NSString *)name
                                        latitude:(CGFloat)latitude
                                       longitude:(CGFloat)longitude {
     
-    return nil;
+    
+    NSDictionary *createLocation = @{
+                                     @"name" : name,
+                                     @"latitude" : @(latitude),
+                                     @"longitude" : @(longitude)
+                                     };
+    return createLocation;
 }
 
 - (NSArray *)namesOfLocations:(NSArray *)locations {
     
-    return nil;
+    NSArray *names = @[];
+    for (NSDictionary *location in locations) {
+        names = [names arrayByAddingObject:location[@"name"]];
+    }
+    return names;
 }
 
 - (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary {
     
-    return nil;
+    BOOL dictionaryIsValid = NO;
+    
+    if ([[dictionary allKeys] containsObject:@"name"] &&
+        [[dictionary allKeys] containsObject:@"latitude"] &&
+        [[dictionary allKeys] containsObject:@"longitude"] &&
+        [dictionary count] == 3 ) {
+        dictionaryIsValid = YES;
+        
+    }
+    return dictionaryIsValid;
+    
 }
 
 - (NSDictionary *)locationNamed:(NSString *)name
                     inLocations:(NSArray *)locations {
     
+    for (NSDictionary *location in locations) {
+        if ([location[@"name"] isEqualToString:name]) {
+            return location;
+        }
+    }
     return nil;
 }
 
