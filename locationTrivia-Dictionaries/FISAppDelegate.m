760a49cbb3e0528aa285300719a67f6dd15d44fb
magfurulabeer <magfurulabeer@gmail.com>
2016-01-05 16:38:58 -0500

    Complete lab

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -16,4 +16,71 @@
  
  */
 
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+    NSString *str;
+    
+    if (length <= [location[@"name"] length]) {
+        str = [location[@"name"] substringToIndex:length];
+    } else {
+        str = location[@"name"];
+    }
+    return str;
+}
+
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    NSDictionary *newLocation = @{@"name"        :   name,
+                                  @"latitude"    :   @(latitude),
+                                  @"longitude"   :   @(longitude)
+                                  };
+    return newLocation;
+}
+
+-(NSArray *)namesOfLocations:(NSArray *)locations {
+    NSMutableArray *leNames = [ @[] mutableCopy ];
+    for (NSDictionary *loc in locations) {
+        [leNames addObject:loc[@"name"]];
+    }
+    return leNames;
+}
+
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)dict {
+    NSArray *keys = [dict allKeys];
+    NSArray *necessaryKeys = @[ @"name", @"latitude", @"longitude" ];
+    
+    if ( [keys count] != 3 ) {
+        return NO;
+    } else {
+        for (NSString *leKey in necessaryKeys) {
+            if ( ![keys containsObject:leKey] ) {
+                return NO;
+            }
+        }
+    }
+    
+    CGFloat lat = [dict[@"latitude"] floatValue];
+    CGFloat lon = [dict[@"longitude"] floatValue];
+    
+    if ( lat < -90.0 || lat > 90) {
+        return NO;
+    }
+    if ( lon < -180.0 || lon > 180.0 ) {
+        return NO;
+    }
+    
+    return YES;
+}
+
+-(NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    NSPredicate *namePredicate = [NSPredicate predicateWithFormat:@"name LIKE %@", name];
+    NSArray *namedLocation = [locations filteredArrayUsingPredicate:namePredicate];
+    if ([namedLocation count] == 1) {
+        return namedLocation[0];
+    }
+    
+    return nil;
+}
+
+
+
+
 @end
