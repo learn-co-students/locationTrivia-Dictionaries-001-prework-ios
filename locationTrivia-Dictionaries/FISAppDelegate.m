68cd9dc6a0c8d59d7fc9fb996201316727114d0d
Dougly <dgalante11@gmail.com>
2016-03-09 20:15:37 -0500

    method one tests pass

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,10 +10,29 @@
     return YES;
 }
 
-/** 
- 
- *  Write your method implementations here.
- 
- */
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length   {
+    NSString *stringToShorten = location[@"name"];
+    if (length > [stringToShorten length]) {
+        return stringToShorten;
+    }
+    stringToShorten = [stringToShorten substringToIndex:length];
+    return stringToShorten;
+}
+
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    return nil;
+}
+
+-(NSArray *)namesOfLocations:(NSArray *)locations {
+    return nil;
+}
+
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary {
+    return nil;
+}
+
+-(NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    return nil;
+}
 
 @end
