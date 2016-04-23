925bc5cdb10a7908a311eaf742755ebb2c5ab18f
dochonglo <ismaelbarry777@hotmail.com>
2016-04-23 00:12:25 -0400

    Lab 19: Location Trivia (Dictionaries) is almost complete.

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -16,4 +16,67 @@
  
  */
 
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+    
+    NSString *returnString = @"";
+    
+    if ([[location allKeys] containsObject:@"name"]) {
+        returnString = [location[@"name"] substringToIndex:length];
+    }
+    
+    return returnString;
+}
+
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    
+    NSMutableDictionary *returnDictionary = [[NSMutableDictionary alloc] init];
+    NSNumber *latitudeNumber = @(latitude);
+    NSNumber *longitudeNumber = @(longitude);
+    
+    [returnDictionary setValue:name forKey:@"name"];
+    [returnDictionary setValue:latitudeNumber forKey:@"latitude"];
+    [returnDictionary setValue:longitudeNumber forKey:@"longitude"];
+    
+    return returnDictionary;
+}
+
+- (NSArray *)namesOfLocations:(NSArray *)locations {
+    
+    NSMutableArray *returnArray = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i=0; i<[locations count]; i++) {
+        [returnArray addObject:locations[i][@"name"]];
+    }
+    
+    return returnArray;
+}
+
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)argument {
+    
+    BOOL isNo = NO;
+    BOOL isYes = YES;
+    
+    if ([argument count] > 3) {
+        return isNo;
+    } else if ([[argument allKeys]containsObject:@"name"] && [[argument allKeys]containsObject:@"latitude"] && [[argument allKeys] containsObject:@"longitude"]) {
+        return isYes;
+    } else {
+        return isNo;
+    }
+
+}
+
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    
+    
+    for (NSDictionary *returnDictionary in locations) {
+        if ([returnDictionary[@"name"] isEqualToString:name]) {
+            
+            return returnDictionary;
+        }
+    }
+    
+    return nil;
+}
+
 @end
