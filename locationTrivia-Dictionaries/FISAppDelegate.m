e93ccc3d1cf82d7c3ced65014f8cb2841caffda2
Cordavi <michaelamundsen@gmail.com>
2016-05-26 16:51:29 -0400

    Wrote logic for methods so all test pass

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,10 +10,46 @@
     return YES;
 }
 
-/** 
- 
- *  Write your method implementations here.
- 
- */
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+    NSString *truncatedName = location[@"name"];
+    
+    if (length >= truncatedName.length) {
+        return truncatedName;
+    }
+    
+    return [truncatedName substringToIndex:length];
+}
+
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    NSMutableDictionary *locationDict = [@{} mutableCopy];
+    locationDict[@"name"] = name;
+    locationDict[@"latitude"] = @(latitude);
+    locationDict[@"longitude"] = @(longitude);
+    
+    return locationDict;
+}
+
+-(NSArray *)namesOfLocations:(NSArray *)locations {
+    NSArray *locationsOnly = [locations valueForKey:@"name"];
+  
+    return locationsOnly;
+}
+
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)locationDict {
+    NSArray *keys = [locationDict allKeys];
+    
+    return [keys count] == 3 && 
+                [keys containsObject:@"name"] &&
+                    [keys containsObject:@"latitude"] &&
+                        [keys containsObject:@"longitude"];
+}
+
+-(NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    NSPredicate *location = [NSPredicate predicateWithFormat:@"name == %@", name];
+    NSDictionary *locationsDict = [[locations filteredArrayUsingPredicate:location]firstObject];
+    
+    
+    return locationsDict;
+}
 
 @end
