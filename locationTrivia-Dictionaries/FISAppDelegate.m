5601ae896f1977a31c0763d968ea1eb49ab5d5d0
hdinhof1 <hdinhof1@binghamton.edu>
2016-06-02 13:25:34 -0400

    Improved readability

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -18,7 +18,7 @@
 
 -(NSString *)stringByTruncatingNameOfLocation:(NSDictionary*)location toLength:(NSUInteger)length {
     NSString * name = location[@"name"];
-    if (length < name.length)
+    if (length < name.length)                                   //is there a way to have one if statement thats ALSO readable
     {
         name = [name substringToIndex:length];
     }
@@ -26,23 +26,32 @@
 }
 
 -(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
-    return @{ @"name" : name, @"latitude" : @(latitude), @"longitude" :@(longitude)};
+    return @{ @"name"      : name,
+              @"latitude"  : @(latitude),
+              @"longitude" : @(longitude)};
 }
 
 -(NSArray *)namesOfLocations:(NSArray *)locations {
-    NSMutableArray *locationNames = [[NSMutableArray alloc] init];
-    NSUInteger i = 0;
-    for (NSDictionary *place in locations) {
+/*  NSMutableArray *locationNames = [[NSMutableArray alloc] init]; //names > locationNames
+    NSUInteger i = 0;                                              //indexing can be replaced with [names addObject:name] method
+    for (NSDictionary *place in locations) {                       //singular variable names, *location in locations
         locationNames[i] = place[@"name"];
         i++;
     }
-    return locationNames;
+*/
+    NSMutableArray *names = [[NSMutableArray alloc] init];
+    for (NSDictionary *location in locations) {
+        NSString *name = location[@"name"];
+        [names addObject:name];
+    }
+
+    return names;
 }
 
--(BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary{
-    BOOL dictionaryIsValid = false;
-    NSArray *keys = [dictionary allKeys];
-    NSUInteger doesEqualThree = keys.count;
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)location{ //location > dictionary
+    BOOL locationIsValid = NO;                           //its more readable if you set YES, then provide edge cases for no
+    NSArray *keys = [location allKeys];
+    NSUInteger lookingForThreeKeys = keys.count;
     NSUInteger three = 3;
     
 /*    
@@ -51,25 +60,39 @@
         dictionaryIsValid = true;
     }
  */
-    if ( [keys containsObject:@"name"] ) {
-        dictionaryIsValid = true;
+    NSNumber *latitudeNumber = location[@"latitude"];
+    CGFloat latitude = [latitudeNumber floatValue];
+    NSNumber *longitudeNumber = location[@"longitude"];
+    CGFloat longitude = [longitudeNumber floatValue];
+    NSString *name = location[@"name"];
+    NSString *tester = @"";
+    BOOL hasLetters = ( [tester length] > 0);
+    
+    if ( [keys containsObject:@"name"] &&
+       ( latitude > -90.0 && latitude < 90.0) &&
+       ( longitude > -180.0 && longitude < 180.0) &&
+       ( lookingForThreeKeys == three)  )
+    {
+        locationIsValid = YES;
     }
-    return (dictionaryIsValid && (doesEqualThree == three));
+    
+      
+    return locationIsValid;
 }
 
 -(NSDictionary *)locationNamed:(NSString *)searchName inLocations:(NSArray *)locations {
-    NSMutableDictionary *returnLocation = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *retLocation = [[NSMutableDictionary alloc] init];
     for (NSDictionary *place in locations) {
         if ([place[@"name"] isEqualToString:searchName]) {
-            returnLocation = [place mutableCopy];
+            retLocation = [place mutableCopy];
         }
     }
     NSArray *namesOfLoc = [self namesOfLocations:locations];
     if ( !([namesOfLoc containsObject:searchName]) ) {
-        returnLocation = nil;
+        retLocation = nil;
     }
     
-/*  Experimenting here in an actual GitSubmit I wouldn't publish this commented out text
+/*  Experimenting here, in an actual GitSubmit I wouldn't publish this commented out text
     NSPredicate *namePredicate = [NSPredicate predicateWithFormat:@"name CONTAINS %@", searchName];
     NSArray *filteredLocations = [locations filteredArrayUsingPredicate:namePredicate];
     
@@ -78,7 +101,9 @@
  */
 
     
-    return returnLocation;
+    return retLocation;
 }
+
+
 
 @end
